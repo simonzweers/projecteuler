e6576be11991c5be4c6c8ec090868109b710c8f3
Simon Zweers <s.zweers21@gmail.com>
2024-07-30 13:44:35 +0200

    Updated double_dabble

--- src/003.c
+++ src/003.c
@@ -15,21 +15,33 @@ void print_binary(uint64_t num, uint8_t count) {
   printf("\n");
 }
 
-uint64_t get_bcd(uint16_t original) {
+uint64_t double_dabble(uint64_t original) {
   // n + 4×ceil(n/3)
   uint64_t num = original;
+  print_binary(num, 64);
+
+  // Determine num original byte size
+  uint8_t num_size = 0;
+  for (uint8_t i = 0; i < 8; i++) {
+    int index = 8 - i - 1;
+    if () {
+      num_size = i + 1;
+      break;
+    }
+  }
+  printf("num_size: %d\n", num_size);
 
   while (1) {
     num = num << 1;
     // Use i as shift position multiplier, so must be range 5-0
-    for (int i = 5; i >= 0; i--) {
+    for (int i = 0; i < 14; i++) {
       uint32_t shift = (2 + i) * 4;
-      uint8_t digit = (num >> shift) & 0b1111;
+      uint32_t digit = (num >> shift) & 0b1111;
       if (digit >= 5) {
         num += (3 << shift);
       }
     }
-    // print_binary(num, 32);
+    print_binary(num, 64);
     if ((num & 0b11111111) == 0)
       break;
   }
@@ -39,10 +51,10 @@ uint64_t get_bcd(uint16_t original) {
 
 int main() {
   uint16_t num = 243;
-  uint64_t numbcd = get_bcd(num);
-  print_binary(numbcd, 32);
-
+  uint64_t numbcd = double_dabble(num);
+  print_binary(numbcd, 64);
+  printf("\n");
   uint16_t num2 = 65244;
-  uint64_t num2bcd = get_bcd(num2);
+  uint64_t num2bcd = double_dabble(num2);
   print_binary(num2bcd, 32);
 }
