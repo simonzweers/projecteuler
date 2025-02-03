e5ba38fe50b039e830fb7d28893d40d2f4b5db24
Simon Zweers <simon.zweers@hva.nl>
2025-02-04 00:23:07 +0100

    add solution for biggass number

--- cpp/003.cpp
+++ cpp/003.cpp
@@ -1,11 +1,12 @@
+#include <cstdint>
 #include <cstdio>
 
-bool isPrime(int input) {
-    int halfInput = input / 2;
+bool isPrime(uint64_t input) {
+    uint64_t halfInput = input / 2;
     if (input <= 3) {
         return true;
     }
-    for (size_t i = 2; i < halfInput + 1; i++) {
+    for (uint64_t i = 2; i < halfInput + 1; i++) {
         // printf("num %d  mod %d \n", input, input % i);
         if (input % i == 0) {
             return false;
@@ -16,11 +17,28 @@ bool isPrime(int input) {
 
 int main() {
     printf("Hello World\n");
-    int inputOriginal = 13195;
-    int input = inputOriginal;
+    uint64_t inputOriginal = 600851475143;
+    // int inputOriginal = 13195;
+    uint64_t input = inputOriginal;
+    uint64_t hafInput = input / 2;
 
-    int prime = 2;
-    for (int i = 1; i < 100; i++) {
-        printf("num: %d is prime: %d\n", i, isPrime(i));
+    uint64_t primeFactor = 0;
+    for (uint64_t i = 2; i < hafInput; i++) {
+        if (isPrime(i)) {
+            // found next prime factor
+
+            if (input % i == 0) {
+                // if input is divisible by prime factor
+                std::printf("factor found: %ld\n", i);
+                input /= i;
+                if (input < i) {
+                    primeFactor = i;
+                    break;
+                }
+            }
+        }
+        printf("num: %ld is prime: %ld\n", i, isPrime(i));
     }
+
+    printf("Found largest prime factor %ld\n", primeFactor);
 }
