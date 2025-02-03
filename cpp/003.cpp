8aec6e4901fdfc7576129d143a3c58214010f432
Simon Zweers <simon.zweers@hva.nl>
2025-02-03 23:50:06 +0100

    added isPrime function

--- cpp/003.cpp
+++ cpp/003.cpp
@@ -1,14 +1,26 @@
 #include <cstdio>
-int main() {
-  printf("Hello World\n");
-  int inputOriginal = 13195;
-  int input = inputOriginal;
 
-  int prime = 2;
-  while (true) {
-    // find next prime
-    if (input % i == 0) {
-      // Division by number will succeed
+bool isPrime(int input) {
+    int halfInput = input / 2;
+    if (input <= 3) {
+        return true;
     }
-  }
+    for (size_t i = 2; i < halfInput + 1; i++) {
+        // printf("num %d  mod %d \n", input, input % i);
+        if (input % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    printf("Hello World\n");
+    int inputOriginal = 13195;
+    int input = inputOriginal;
+
+    int prime = 2;
+    for (int i = 1; i < 100; i++) {
+        printf("num: %d is prime: %d\n", i, isPrime(i));
+    }
 }
