5e18a35bba892d53b71d5a6ebf35adc397da96ad
Simon Zweers <simon.zweers@hva.nl>
2025-02-09 16:17:48 +0100

    add solulu

--- cpp/005.cpp
+++ cpp/005.cpp
@@ -4,15 +4,25 @@
 #include <cstdint>
 #include <cstdio>
 
-int main() {
-    int maxDivNumber = 10;
-    int beginnumber = maxDivNumber * 2;
-    while (true) {
-        // Check all numbers from 1 to maxDivNumber for division
-        for (int i = 1; i <= maxDivNumber; i++) {
-            if (beginnumber % i == 0) {
-                printf("sdaw");
-            }
-        }
+bool check_numbers(uint32_t input, uint32_t max_num) {
+  for (int i = 2; i <= max_num; i++) {
+    if (input % i != 0) {
+      return false;
     }
+  }
+  return true;
+}
+
+int main() {
+  int maxDivNumber = 20;
+  int beginnumber = maxDivNumber * 2;
+  int scratch = beginnumber;
+  while (true) {
+    bool res = check_numbers(scratch, maxDivNumber);
+    if (res) {
+      printf("brih %d %d\n", scratch, res);
+      break;
+    }
+    scratch++;
+  }
 }
