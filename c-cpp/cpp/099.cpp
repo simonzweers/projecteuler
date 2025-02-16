77e53529901bb4cae99701baecaa9002ff63a536
Simon Zweers <simon.zweers@hva.nl>
2025-02-16 15:26:53 +0100

    separate euler99 into fiufunctions

--- c-cpp/cpp/099.cpp
+++ c-cpp/cpp/099.cpp
@@ -24,7 +24,25 @@ double get_log(base_exp_t &p) {
     return (double)p.exponent * log10((double)p.base);
 }
 
-int main() {
+uint32_t euler99(std::vector<base_exp_t> &pairs) {
+
+    double highest = 0;
+    int linei = 1;
+    int line = 0;
+    for (auto &pair : pairs) {
+        double res = get_log(pair);
+        // std::printf("b: %ld, e: %ld, log %f\n", pair.base, pair.exponent,
+        // res);
+        if (res > highest) {
+            highest = res;
+            line = linei;
+        }
+        linei++;
+    }
+    return line;
+}
+
+int read_file(std::vector<base_exp_t> &pairs) {
     std::ifstream f("./res/base_exp.txt");
     if (!f.is_open()) {
         std::cerr << "Error opening the file!";
@@ -32,7 +50,6 @@ int main() {
     }
 
     std::string s;
-    std::vector<base_exp_t> pairs;
     while (std::getline(f, s)) {
         std::string t;
         std::stringstream ss{s};
@@ -46,31 +63,22 @@ int main() {
         pairs.push_back(pair_tmp);
         // std::printf("b: %ld, e: %ld\n", pair_tmp.base, pair_tmp.exponent);
     }
+    f.close();
+    return 0;
+}
+
+int main() {
 
     uint32_t num_threads = std::thread::hardware_concurrency();
     std::printf("Number of threads: %d\n", num_threads);
-
+    std::vector<base_exp_t> pairs;
+    read_file(pairs);
     auto t1 = high_resolution_clock::now();
-
-    double highest = 0;
-    int linei = 1;
-    int line;
-    for (auto &pair : pairs) {
-        double res = get_log(pair);
-        // std::printf("b: %ld, e: %ld, log %f\n", pair.base, pair.exponent,
-        // res);
-        if (res > highest) {
-            highest = res;
-            line = linei;
-        }
-        linei++;
-    }
-
+    uint32_t line = euler99(pairs);
     auto t2 = high_resolution_clock::now();
     auto ms_int = duration_cast<nanoseconds>(t2 - t1);
 
     std::printf("Line with highest result: %d\n", line);
     std::printf("duration %lfms\n", ((double)ms_int.count()) * (pow(10, -6)));
-    f.close();
     return 0;
 }
